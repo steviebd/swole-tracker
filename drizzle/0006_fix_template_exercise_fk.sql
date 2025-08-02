2b3f6b7576f025875f5af6be25c9471a8b495653
“Stevie <Stevenbduong@gmail.com>
2025-08-02 16:15:22 +1000

    consolidate sql setup

--- drizzle/0006_fix_template_exercise_fk.sql
+++
@@ -1,6 +0,0 @@
--- Fix foreign key constraint for templateExerciseId to allow template updates
--- Drop the existing foreign key constraint
-ALTER TABLE "swole-tracker_session_exercise" DROP CONSTRAINT "swole-tracker_session_exercise_templateExerciseId_swole-tracker";--> statement-breakpoint
-
--- Add the new foreign key constraint with SET NULL on delete
-ALTER TABLE "swole-tracker_session_exercise" ADD CONSTRAINT "swole-tracker_session_exercise_templateExerciseId_swole-tracker" FOREIGN KEY ("templateExerciseId") REFERENCES "swole-tracker_template_exercise"("id") ON DELETE set null;
--- drizzle/0009_add_linking_rejected.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Add linkingRejected column to template_exercise table
--- This column tracks if user explicitly chose not to link a template exercise to a master exercise
-
-ALTER TABLE "swole-tracker_template_exercise" 
-ADD COLUMN "linkingRejected" boolean NOT NULL DEFAULT false;
-
--- Add comment for documentation
-COMMENT ON COLUMN "swole-tracker_template_exercise"."linkingRejected" 
-IS 'Track if user explicitly chose not to link this template exercise to a master exercise';
